22bfab176dc2c5a2b98df42af5dc2b7565ef39c9
Joel <xxx4bn3rxxx@gmail.com>
2020-11-01 01:01:02 -0500

    Ended DOM exercises page

---
+++ js/hamburguesa.js
@@ -0,0 +1,12 @@
+export default function hamburgerMenu(panelBtn, panel, panelLink) {
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+      document.querySelector(panel).classList.toggle("is-active");
+      document.querySelector(panelBtn).classList.toggle("is-active");
+    }
+    if (e.target.matches(panelLink)) {
+      document.querySelector(panel).classList.remove("is-active");
+      document.querySelector(panelBtn).classList.remove("is-active");
+    }
+  });
+}
---
+++ js/index.js
@@ -0,0 +1,5 @@
+import hamburgerMenu from "./hamburguesa.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  hamburgerMenu(".panel-btn", ".panel", ".panel a");
+});
